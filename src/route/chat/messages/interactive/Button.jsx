45be2added269e6455dd4a2c6d682a923d5971ac
elguesabal <joseelguesabal@gmail.com>
2026-05-12 21:42:29 -0300

    adicionando renderizacao de header na mensagem do tipo button com os tipos text, image, video e document adicionando renderizacao de body na mensagem do tipo button de forma simples (ainda adicionar mais estilizacoes) adicionando renderizacao de footer na mensagem do tipo button de forma simples (ainda adicionar mais estilizacoes) refatorando codigo corrigindo bugs

--- src/route/chat/messages/interactive/Button.jsx
+++ src/route/chat/messages/interactive/Button.jsx
@@ -1,22 +1,80 @@
-// import Text from "../Text.jsx";
-// import Image from "../context/Image.jsx";
-// import Video from "../context/Video.jsx";
-// import Document from "../context/Document.jsx";
+import { memo, useMemo, useState, useRef } from "react";
+
+import Image from "../Image.jsx";
+import Video from "../Video.jsx";
+import Document from "../Document.jsx";
+
+/**
+ * @author VAMPETA
+ * @brief HEADER DA MENSAGEM INTERACTIVE DO TIPO BUTTON
+ * @param {Object} message MENSAGEM A SER RENDERIZADA
+*/
+function Header({ message }) {
+	switch (message.data.interactive.header?.type) {
+		case "text":
+			return (<p>{message.data.interactive.header.text}</p>);
+		case "image":
+			return (<Image message={{ data: message.data.interactive.header, direction: "outbound" }} />);
+		case "video":
+			return (<Video message={{ data: message.data.interactive.header, direction: "outbound" }} />);
+		case "document":
+			return (<Document message={{ data: message.data.interactive.header, direction: "outbound" }} />);
+		default:
+			return (null);
+	}
+}
+
+/**
+ * @author VAMPETA
+ * @brief BODY DA MENSAGEM INTERACTIVE DO TIPO BUTTON
+ * @param {Object} message MENSAGEM A SER RENDERIZADA
+*/
+function Body({ message }) {
+	return (
+		<p>{message.data.interactive.body.text}</p>
+	);
+}
+
+/**
+ * @author VAMPETA
+ * @brief FOOTER DA MENSAGEM INTERACTIVE DO TIPO BUTTON
+ * @param {Object} message MENSAGEM A SER RENDERIZADA
+*/
+function Footer({ message }) {
+	return (
+		<span>{message.data.interactive.footer?.text}</span>
+	);
+}
+
+/**
+ * @author VAMPETA
+ * @brief BUTTONS DA MENSAGEM INTERACTIVE DO TIPO BUTTON
+ * @param {Object} message MENSAGEM A SER RENDERIZADA
+*/
+function Buttons({ message }) {
+	return (
+		<div className="flex gap-2 text-white">
+			{message.data.interactive.action.buttons.map((button, i) => (
+				<button className="flex-1 bg-orange-500 rounded hover:bg-orange-400 transition cursor-not-allowed" key={i}>{button.reply.title}</button>
+			))}
+		</div>
+	);
+}
 
 /**
  * @author VAMPETA
  * @brief RENDERIZA MENSAGEM DE INTERACTIVE DO TIPO BUTTON
  * @param {Object} message MENSAGEM A SER RENDERIZADA
 */
-export default function Button({ message }) {
+const Button = memo(function Button({ message }) {
 	return (
 		<div>
-			{message.data.interactive.body.text}
-			<div className="flex gap-2 text-white">
-				{message.data.interactive.action.buttons.map((button) => (
-					<button className="flex-1 bg-orange-500 rounded hover:bg-orange-400 transition cursor-not-allowed">{button.reply.title}</button>
-				))}
-			</div>
+			<Header message={message} />
+			<Body message={message} />
+			<Footer message={message} />
+			<Buttons message={message} />
 		</div>
 	);
-}
+});
+
+export default Button;
